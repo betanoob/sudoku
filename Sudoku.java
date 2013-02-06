d535e884afe5a3d2ae2009aa6d7098ca398788bc
Scott Cantisani <scantisani@gmail.com>
2013-02-06 16:00:38 +0000

    pre-made Sudoku in initBoard()

--- Sudoku.java
+++ Sudoku.java
@@ -2,13 +2,13 @@
 * Project: 			Sudoku
 ************************************************
 *
-* Authors: 			Claire Giry
-*					Hans-Peter Hoellwirth
-*					Scott Cantisani
-*					Simranbir Singh
+* Authors: 		Claire Giry
+*			Hans-Peter Hoellwirth
+*			Scott Cantisani
+*			Simranbir Singh
 *
 * Creation date:	06.02.2013
-* Last updated:     06.02.2013
+* Last updated:		06.02.2013
 ***********************************************/
 public class Sudoku {
 
@@ -23,11 +23,45 @@ public class Sudoku {
 	}
 
 	public void initBoard () {
-		// TBD
+		int[][] temp = {
+			{1, 0, 0, 0, 8, 0, 3, 9, 0}, 
+			{0, 0, 0, 0, 1, 0, 0, 0, 0},
+			{0, 9, 7, 3, 6, 0, 8, 5, 0},
+			{8, 2, 4, 0, 3, 9, 0, 0, 0},
+			{0, 3, 0, 0, 0, 0, 0, 2, 0},
+			{0, 0, 0, 4, 5, 0, 9, 8, 3},
+			{0, 4, 2, 0, 9, 1, 5, 6, 0},
+			{0, 0, 0, 0, 2, 0, 0, 0, 0},
+			{0, 7, 1, 0, 4, 0, 0, 0, 8}
+		};
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				board[i][j] = temp[i][j];
+			}
+		}
 	}
 
 	public void printBoard () {
-		// TBD
+		//This is only a rough print function for testing whether initBoard() was working -- not done
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+
+				if (board[i][j] == 0) {
+					System.out.print("*" + " ");
+				} else {
+					System.out.print(board[i][j] + " ");
+				}
+
+				if (j % 3 == 2) {
+					System.out.print("|");
+				}
+			}
+			System.out.println();
+			if (i % 3 == 2) {
+				System.out.println("---------------------");
+			}
+		}
 	}
 
 	public void solveBoard () {
